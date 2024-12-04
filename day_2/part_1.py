c2fc7623eaca5b94ef4781a829a808cfe1dd79ff
Thomas Maddox <me@thomasem.net>
2024-12-04 08:52:04 -0600

    minor day 2 refactor for fun

---
+++ day_2/part_1.py
@@ -0,0 +1,18 @@
+#!/usr/bin/env python
+
+from load import load_input
+from safe import safe
+
+
+def main():
+    reports = load_input()
+    safe_count = 0
+    for report in reports:
+        if safe(report):
+            safe_count += 1
+
+    print(safe_count)
+
+
+if __name__ == "__main__":
+    main()
--- day_2/safe.py
+++ day_2/safe.py
@@ -1,8 +1,17 @@
-#!/usr/bin/env python
+def safe_brute(report):
+    diffs = [report[i+1] - report[i] for i in range(len(report) - 1)]
+    return all(0 < diff < 4 for diff in diffs) or all(-4 < diff < 0 for diff in diffs)
 
-import sys
 
-from load import load_input
+def safe_brute_dampener(report):
+    if safe_brute(report):
+        return True
+    
+    for i in range(len(report)):
+        if safe_brute(report[:i] + report[i+1:]):
+            return True 
+
+    return False
 
 
 def safe(report, tolerance=0):
@@ -18,28 +27,10 @@ def safe(report, tolerance=0):
 
         if abs(diff) > 3 or diff == 0 or trend * diff < 0:
             # handle edge case where first value needs to be removed
-            # and try removing this value and the next one
+            # because it's the value that sets the trend;
+            # also try removing this value and the next one
             return safe(report[1:], tolerance - 1) \
                 or safe(report[:i] + report[i + 1:], tolerance - 1) \
                 or safe(report[:i+1] + report[i + 2:], tolerance - 1)
 
     return True
-
-
-def main():
-    reports = load_input()
-    safe_count = 0
-
-    tolerance = 0
-    if (len(sys.argv) > 1):
-        tolerance = int(sys.argv[1])
-
-    for report in reports:
-        if safe(report, tolerance=tolerance):
-            safe_count += 1
-
-    print(safe_count)
-
-
-if __name__ == "__main__":
-    main()
