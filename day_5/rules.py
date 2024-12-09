c5dc99b7a3c0ceb5b22c060fce8399b864e00181
Thomas Maddox <me@thomasem.net>
2024-12-08 19:24:26 -0600

    day 5 rules_lookup list -> set

--- day_5/rules.py
+++ day_5/rules.py
@@ -1,6 +1,7 @@
 def to_rules_lookup(rules):
     rules_lookup = {}
     for rule in rules:
-        rule_entry = rules_lookup.get(rule[0], [])
-        rules_lookup[rule[0]] = rule_entry + [rule[1]]
+        rule_entry = rules_lookup.get(rule[0], set())
+        rule_entry.add(rule[1])
+        rules_lookup[rule[0]] = rule_entry
     return rules_lookup
